b7946113f8d9d5487ca0389cc9fc9863ea9e7436
Fraser Hart <fraser@fraser-hart.co.uk>
2020-08-15 13:55:43 +0100

    extracting magic words

--- bin/numbersToWords.js
+++ bin/numbersToWords.js
@@ -36,11 +36,15 @@ const TENS = [
 const MIN_INPUT = 0;
 const MAX_INPUT = 100000;
 
+const HUNDRED = "hundred";
+const THOUSAND = "thousand";
+const AND = "and";
+
 const numbersToWords = (number) => {
   if (isNaN(number) || number < MIN_INPUT || number > MAX_INPUT)
     return "invalid input";
 
-  if (number === 0) return "zero";
+  if (number == 0) return "zero";
 
   const numString = "" + number;
 
@@ -54,19 +58,20 @@ const numbersToWords = (number) => {
   }
 
   if (numString < 1000) {
-    if (numString % 100 === 0) return `${numbersToWords(numString[0])} hundred`;
-    return `${numbersToWords(numString[0])} hundred and ${numbersToWords(
+    if (numString % 100 === 0)
+      return `${numbersToWords(numString[0])} ${HUNDRED}`;
+    return `${numbersToWords(numString[0])} ${HUNDRED} ${AND} ${numbersToWords(
       numString - numString[0] * 100
     )}`;
   }
 
   if (numString % 1000 === 0)
-    return `${numbersToWords(numString / 1000)} thousand`;
+    return `${numbersToWords(numString / 1000)} ${THOUSAND}`;
 
-  const AND = numString.slice(-3) < 100 ? "and " : "";
+  const OPTIONAL_AND = numString.slice(-3) < 100 ? AND + " " : "";
   return `${numbersToWords(
     numString.slice(0, -3)
-  )} thousand ${AND}${numbersToWords(numString.slice(-3))}`;
+  )} ${THOUSAND} ${OPTIONAL_AND}${numbersToWords(numString.slice(-3))}`;
 };
 
 module.exports = numbersToWords;
