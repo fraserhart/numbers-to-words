f9f0d2c022be259775f0e65c8d8139c4919c6f46
Fraser Hart <fraser@fraser-hart.co.uk>
2020-08-15 14:10:04 +0100

    update require

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-const numbersToWords = require("../bin/numbersToWords");
+const numbersToWords = require("./numbersToWords");
 var args = process.argv.slice(2);
 console.log(numbersToWords(args[0]));
